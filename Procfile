abd6b9b06c4710bfe34d89bf0f28283a7963e7f9
mahirairgn <mahirairgn@gmail.com>
2024-05-18 15:18:53 +0700

    mencoba deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn marmut.wsgi --log file-
+web: gunicorn 'marmut.wsgi'
--- marmut/settings.py
+++ marmut/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,9 +135,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
